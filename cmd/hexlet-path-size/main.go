c7f58b4a22797257a8f65a2c3274bf5c898db2bc
Vasiliy Aleksandrov <alex.v.a@inbox.ru>
2025-12-18 13:26:03 +0300

    s-2 add help text

--- cmd/hexlet-path-size/main.go
+++ cmd/hexlet-path-size/main.go
@@ -1,10 +1,36 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"os"
+
+	"github.com/urfave/cli/v3"
 )
 
 func main() {
-	myString := "Hello from Hexlet!"
-	fmt.Println(myString)
+	cmd := &cli.Command{
+		Name:  "hexlet-path-size",
+		Usage: "print size of a file or directory",
+
+		Action: func(ctx context.Context, cmd *cli.Command) error {
+			fmt.Println("Hello from Hexlet!")
+			return nil
+		},
+	}
+
+	cli.RootCommandHelpTemplate = `
+	./bin/hexlet-path-size
+	
+	NAME:
+		hexlet-path-size - print size of a file or directory
+	
+	USAGE:
+		hexlet-path-size [global options]
+	
+	GLOBAL OPTIONS:
+		--help, -h  show help
+`
+
+	cmd.Run(context.Background(), os.Args)
 }
