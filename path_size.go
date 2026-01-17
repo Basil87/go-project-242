b9195206a1e15a43d2c2abf0408534af6e336577
Vasiliy Aleksandrov <alex.v.a@inbox.ru>
2026-01-17 11:50:46 +0300

    s-4 fix paths and add tests to actions

--- path_size.go
+++ path_size.go
@@ -1,1 +1,28 @@
-package go_project_242
+package code
+
+import (
+"os"
+"path/filepath"
+)
+
+func GetSize(path string) (int64, error) {
+	var total int64
+
+	err := filepath.Walk(path, func(p string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if !info.IsDir() {
+			total += info.Size()
+		}
+
+		return nil
+	})
+
+	if err != nil {
+		return 0, err
+	}
+
+	return total, nil
+}
