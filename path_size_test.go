ad72dc240b1db55fd822afa98540c63780a7382b
Vasiliy Aleksandrov <alex.v.a@inbox.ru>
2026-01-27 10:28:28 +0300

    s-fix-1 add godoc

--- path_size_test.go
+++ path_size_test.go
@@ -13,7 +13,7 @@ func TestGetSize(t *testing.T) {
 	t.Run("file", func(t *testing.T) {
 		path := filepath.Join(testdata, "test.txt")
 
-		size, err := GetSize(path, false, false)
+		size, err := getSize(path, false, false)
 
 		require.NoError(t, err)
 		require.Equal(t, int64(17), size)
@@ -22,7 +22,7 @@ func TestGetSize(t *testing.T) {
 	t.Run("empty file", func(t *testing.T) {
 		path := filepath.Join(testdata, "empty.txt")
 
-		size, err := GetSize(path, false, false)
+		size, err := getSize(path, false, false)
 
 		require.NoError(t, err)
 		require.Equal(t, int64(0), size)
@@ -31,7 +31,7 @@ func TestGetSize(t *testing.T) {
 	t.Run("directory", func(t *testing.T) {
 		path := "testdata"
 
-		size, err := GetSize(path, false, false)
+		size, err := getSize(path, false, false)
 
 		require.NoError(t, err)
 		require.Equal(t, int64(34), size)
@@ -40,7 +40,7 @@ func TestGetSize(t *testing.T) {
 	t.Run("directory recursive", func(t *testing.T) {
 		path := "testdata"
 
-		size, err := GetSize(path, false, true)
+		size, err := getSize(path, false, true)
 
 		require.NoError(t, err)
 		require.Equal(t, int64(71), size)
@@ -49,7 +49,7 @@ func TestGetSize(t *testing.T) {
 	t.Run("directory recursive with hidden files", func(t *testing.T) {
 		path := "testdata"
 
-		size, err := GetSize(path, true, true)
+		size, err := getSize(path, true, true)
 
 		require.NoError(t, err)
 		require.Equal(t, int64(84), size)
@@ -58,7 +58,7 @@ func TestGetSize(t *testing.T) {
 	t.Run("not exists", func(t *testing.T) {
 		path := filepath.Join(testdata, "unknown")
 
-		_, err := GetSize(path, false, false)
+		_, err := getSize(path, false, false)
 
 		require.Error(t, err)
 	})
